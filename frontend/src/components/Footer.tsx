e58f8549252384a9299f097175ff0a8753ec7a79
ejminguez <ebminguez@up.edu.ph>
2025-07-03 00:47:41 +0800

    feat: footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import flower4 from "../assets/pictures/flower-4.png";
+
+const Footer = () => {
+  return (
+    <section className="relative overflow-clip h-64 flex justify-center items-center mt-20">
+      <h5 className="text-center text-white top-10 relative">@habilin2025</h5>
+      {/* WHITE GRADIENT */}
+      <div className="absolute bottom-0 w-full h-24 z-20 bg-gradient-to-t from-white to-transparent" />
+      {/* BACKGROUND FLOWER */}
+      <img src={flower4} loading="lazy" className="absolute top-0 -z-50" />
+    </section>
+  );
+};
+
+export default Footer;
