ca7f313cbd0eeabae479931d56cb445ac4848719
ejminguez <ebminguez@up.edu.ph>
2025-07-03 00:50:22 +0800

    feat: routing changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ScrollToTopOnRouteChange from "./wrapper/ScrollToTopOnRouteChange.tsx";
+import Footer from "./components/Footer.tsx";
 
 const App = () => {
   return (
     <main className="flex flex-col">
       <Navbar />
-      <section className="top-[10vh] relative min-h-screen">
+      <ScrollToTopOnRouteChange />
+      <section className="top-[10vh] relative min-h-screen overflow-x-clip">
         <Outlet />
       </section>
+      <Footer />
     </main>
   );
 };
--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App.tsx";
-import Home from "../pages/Home.tsx";
-import ErrorNotFound from "../pages/ErrorNotFound.tsx";
+import App from "@/App.tsx";
+import Home from "@/pages/Home.tsx";
+import ErrorNotFound from "@/pages/ErrorNotFound.tsx";
+import Gallery from "@/pages/Gallery.tsx";
+import Letters from "@/pages/Letters.tsx";
+import Playlist from "@/pages/Playlist";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +15,18 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "/alaala",
+        element: <Gallery />,
+      },
+      {
+        path: "/liham",
+        element: <Letters />,
+      },
+      {
+        path: "/himig",
+        element: <Playlist />,
       },
     ],
   },
