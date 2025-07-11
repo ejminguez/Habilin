8a80a116761c91b69b935518d245b0f7224c7344
ejminguez <ebminguez@up.edu.ph>
2025-07-11 19:25:31 +0800

    feat: added animations

--- frontend/src/components/Home/Greetings.tsx
+++ frontend/src/components/Home/Greetings.tsx
@@ -1,11 +1,41 @@
 import flower4 from "@/assets/pictures/flower-4-medium.webp";
+import gsap from "gsap";
+import TextPlugin from "gsap/TextPlugin";
+import { useEffect, useRef } from "react";
+
+gsap.registerPlugin(TextPlugin);
 
 const Greetings = () => {
+  const typewriterRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    const messages = ["HAPPY ANNIVERSARY", "I LOVE YOU"];
+
+    let index = 0;
+
+    const loopTyping = () => {
+      const currentText = messages[index];
+
+      gsap.to(typewriterRef.current, {
+        duration: 2.5,
+        text: currentText,
+        ease: "sine",
+        onComplete: () => {
+          gsap.delayedCall(2, () => {
+            index = (index + 1) % messages.length;
+            loopTyping();
+          });
+        },
+      });
+    };
+
+    loopTyping();
+  }, []);
   return (
     <section className="relative min-h-screen flex flex-col items-center">
       <div className="flex flex-col items-center text-center">
         <h1 className="font-lore text-[3rem] text-habilin-red">
-          Happy Anniversary
+          <span ref={typewriterRef} />
         </h1>
         <p className="font-reenie text-[3rem]">my love</p>
       </div>
--- frontend/src/wrapper/ScrollToTopOnRouteChange.tsx
+++ frontend/src/wrapper/ScrollToTopOnRouteChange.tsx
@@ -1,19 +1,27 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { ScrollSmoother } from "gsap/all";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import gsap from "gsap";
 
 const ScrollToTopOnRouteChange = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    const smoother = ScrollSmoother.get();
+    const scrollToTop = () => {
+      const smoother = ScrollSmoother.get();
 
-    if (smoother) {
-      smoother.scrollTo(1, true); // true = animate
-    } else {
-      // Fallback just in case smoother isn't initialized yet
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    }
+      if (smoother) {
+        // slight delay ensures ScrollSmoother is fully initialized
+        gsap.delayedCall(0.05, () => {
+          smoother.scrollTo(0, true);
+        });
+      } else {
+        // fallback for non-smooth pages
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }
+    };
+
+    scrollToTop();
   }, [pathname]);
 
   return null;
